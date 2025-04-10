b307962e8f6eba3e7768d3cbb390904694b3ce8f
asthetik <asthetik.sun@outlook.com>
2025-04-10 15:18:51 +0800

    Update algorithm exercises

--- exercises/algorithm/algorithm4.rs
+++ exercises/algorithm/algorithm4.rs
@@ -3,7 +3,6 @@
 	This problem requires you to implement a basic interface for a binary tree
 */
 
-//I AM NOT DONE
 use std::cmp::Ordering;
 use std::fmt::Debug;
 
@@ -50,13 +49,25 @@ where
 
     // Insert a value into the BST
     fn insert(&mut self, value: T) {
-        //TODO
+        match self.root {
+            None => self.root = Some(Box::new(TreeNode::new(value))),
+            Some(ref mut node) => node.insert(value),
+        }
     }
 
     // Search for a value in the BST
     fn search(&self, value: T) -> bool {
-        //TODO
-        true
+        let mut head = &self.root;
+        while let Some(node) = head {
+            if value == node.value {
+                return true;
+            } else if value < node.value {
+                head = &node.left;
+            } else {
+                head = &node.right;
+            }
+        }
+        false
     }
 }
 
@@ -66,7 +77,17 @@ where
 {
     // Insert a node into the tree
     fn insert(&mut self, value: T) {
-        //TODO
+        if value < self.value {
+            match self.left {
+                Some(ref mut node) => node.insert(value),
+                None => self.left = Some(Box::new(TreeNode::new(value))),
+            }
+        } else if value > self.value {
+            match self.right {
+                Some(ref mut node) => node.insert(value),
+                None => self.right = Some(Box::new(TreeNode::new(value))),
+            }
+        }
     }
 }
 
